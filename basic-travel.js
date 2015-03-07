598781e7406cbb8f4d05c93cf641b6b0e191452e
livvyjeffsgit <olivia@tribesay.com>
2015-03-07 16:03:23 +0700

    all input fields working

--- basic-travel.js
+++ basic-travel.js
@@ -7,9 +7,8 @@ Router.map(function(){
 });
 
 if (Meteor.isClient) {
-  // counter starts at 0
 
-  Session.setDefault('counter', 0);
+  Meteor.subscribe('theItinerary');
 
   Template.itineraries.helpers({
     'itinerary': function(){
@@ -18,42 +17,54 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.formFirst.helpers({
-
-  });
-
   Template.formFirst.events({
     'submit form': function(event){
       event.preventDefault();
       Router.go('additionalInfo');
-      // var currentUserId = Meteor.userId();
-      ItineraryList.insert({
-        when: event.target.itinWhen.value,
-        length: event.target.itinLength.value,
-        budget: event.target.itinBudget.value
-        // createdBy: currentUserId
-      });
+      Session.set('itinWhen',event.target.itinWhen.value);
+      Session.set('itinLength',event.target.itinLength.value);
+      Session.set('itinBudget',event.target.itinBudget.value);
     }
   });
 
   Template.additionalInfo.events({
     'submit form': function(event){
       event.preventDefault();
-      Router.go('confirmation');
-      // var currentUserId = Meteor.userId();
-      ItineraryList.insert({
-        name: event.target.itinName.value,
-        email: event.target.itinEmail.value,
-        description: event.target.itinDescription.value
-        // createdBy: currentUserId
-      });
+      var when = Session.get('itinWhen');
+      var length = Session.get('itinLength');
+      var budget = Session.get('itinBudget');
+      var name = event.target.itinName.value;
+      var email = event.target.itinEmail.value;
+      var description = event.target.itinDescription.value;
+      Meteor.call('createNewItinerary',when,length,budget,name,email,description);
+    
+      Router.go('confirmation'); 
     }
   });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+
+  Meteor.publish('theItinerary',function(){
+    return ItineraryList.find();
   });
+
+  Meteor.methods({
+    'createNewItinerary': function(when,length,budget,name,email,description){
+      var userId = Meteor.userId();
+      ItineraryList.insert({
+        when: when,
+        length: length,
+        budget: budget,
+        name: name,
+        email: email,
+        description: description,
+        createdBy: userId
+      });
+
+    }
+
+  });
+
 }
