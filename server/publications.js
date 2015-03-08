d15338bc36103dd66e9bf362f0a7258407eebeb7
livvyjeffsgit <olivia@tribesay.com>
2015-03-08 16:07:12 +0700

    verification to my email address
    look into adding roles - later down the line

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,14 @@
   Meteor.publish('theItinerary',function(){
   	var currentUserId = this.userId;
-  	if(currentUserId === 'P5x3cNHy7AuLwdG6J'){
-      //un: jeffers.olivia@gmail.com
+  	// if(currentUserId === 'P5x3cNHy7AuLwdG6J'){
+  		if(this.userId){
+
+  			var user = Meteor.users.findOne(this.userId);
+  			if(user.emails[0].address === 'jeffers.olivia@gmail.com'){
+  				      //un: jeffers.olivia@gmail.com
       //pw: ojeffers7
       return ItineraryList.find();
   }
+
+}
 });
